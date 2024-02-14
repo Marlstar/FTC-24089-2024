e86904524251b2f1c3eb61ede0f8ed246b07d0eb
Hchap1 <58793111+hchap1@users.noreply.github.com>
2024-02-14 12:51:00 +1000

    Add files via upload

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/Robot.java
@@ -4,6 +4,14 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+// All the imports the IMU wanted:
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
 public class Robot {
     public ControllerInput controllerInput;
     HardwareMap hardwareMap;
@@ -51,6 +59,46 @@ public class Robot {
             }
         } public Motors motors;
 
+        public class IMU {
+            public BNO055IMU imu; //define IMU (for GYRO).
+            public double targetYaw;
+            // YAW position
+            // This will be modified by the user with joysticks, and the robot will use a
+            // PID controller to align itself with the target YAW.
+
+            public void resetIMU(){
+                // Just a wrapper function to look nice
+                initIMU();
+            }
+
+            public void initIMU() {
+                // Reset and initalize the IMU.
+                // It wanted parameters so I create a new parameter obj
+                BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+                parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+                parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+                parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+                parameters.loggingEnabled = true;
+                parameters.loggingTag = "IMU";
+                parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+                imu = hardwareMap.get(BNO055IMU.class, "imu");
+                // This resets it
+                imu.initialize(parameters);
+            }
+            // This is copied from my block code last year.
+            public double getYawDegrees() {
+                Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                return AngleUnit.DEGREES.normalize(angles.firstAngle);
+            }
+
+            // Another wrapper function
+            public void init() {
+                initIMU();
+            }
+
+        } public IMU robotIMU;
+
+
         public class Movement {
             class DesiredMovements {
                 public double frontLeftDrive;
@@ -59,20 +107,40 @@ public class Robot {
                 public double backRightDrive;
             } public DesiredMovements desiredMovements;
 
+            public double calculateRotationPower(double targetRotation, double threshold) {
+                double currentRotation = robotIMU.getYawDegrees();
+                double deltaRotation = targetRotation - currentRotation;
+                // Such that if the robot is at 15d and the targetRotation = 0d,
+                // this will be equal to -15d. For now I'll use a > or < but once
+                // it works, I will switch to PID.
+
+                if (deltaRotation < -threshold) { return -0.2; }
+                if (deltaRotation >  threshold) { return  0.2; }
+                else { return 0.0; }
+            }
+
             void calculateMovementTele() {
                 double mx = controllerInput.movement_x;
                 double my = controllerInput.movement_y;
-                double r = controllerInput.rotation;
+                //double r = controllerInput.rotation; <- Leave commented out for now
+                double r = calculateRotationPower(0.0, 3.0);
+
+                // For now disregard R and focus on making YAW = 0
 
                 // Denominator is the largest motor power (absolute value) or 1
                 // This ensures all the powers maintain the same ratio, but only when
                 // at least one is out of the range [-1, 1]
                 double denominator = Math.max(Math.abs(my) + Math.abs(mx) + Math.abs(r), 1);
 
-                desiredMovements.frontLeftDrive = (mx + my + r) / denominator;
-                desiredMovements.frontRightDrive = (mx - my - r) / denominator;
-                desiredMovements.backLeftDrive = (mx - my + r) / denominator;
-                desiredMovements.backRightDrive = (mx + my - r) / denominator;
+                //If we are correcting yaw, slow down the motors so that it makes a difference
+                // (If they are all 100% speed then decrease to 80% so that the motors can speed up
+                // more to rotate). Learnt this last year.
+                double movementMultiplier = 1.0 - Math.abs(r);
+
+                desiredMovements.frontLeftDrive = ((mx + my) * movementMultiplier + r) / denominator;
+                desiredMovements.frontRightDrive = ((mx - my) * movementMultiplier - r) / denominator;
+                desiredMovements.backLeftDrive = ((mx - my) * movementMultiplier + r) / denominator;
+                desiredMovements.backRightDrive = ((mx + my) * movementMultiplier - r) / denominator;
             }
 
             public void setMotorPowers()
